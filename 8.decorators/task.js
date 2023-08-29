66068bda34132f878d87546c597812d7ab314c9d
ludens <tatyana.lew4encko@yandex.ru>
2023-08-29 19:11:42 +0600

    выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.result);
+			return "Из кеша: " + objectInCache.result;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash,
+			result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	let isTrottled = false;
+
+	function wrapper(...args) {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			func(args);
+			wrapper.count++;
+		}, delay);
+		if (!isTrottled) {
+			func(...args);
+			wrapper.count++;
+			isTrottled = true;
+		}
+		wrapper.allCount++;
+	}
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+	return wrapper;
 }
